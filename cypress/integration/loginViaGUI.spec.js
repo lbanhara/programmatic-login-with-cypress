b2148bc3624b0d06bc72a0c434d7ff3d4168f9bf
Liana Mara Cunha Banhara <p017250@rede.sp>
2022-06-21 17:32:28 -0300

    backup

--- cypress/integration/loginViaGUI.spec.js
+++ cypress/integration/loginViaGUI.spec.js
@@ -2,5 +2,26 @@ it('successfully logs in via GUI', () => {
   cy.intercept('GET', `${Cypress.env('apiUrl')}/Autenticacao/Ldap`).as('getUserModels')
   cy.loginViaGUI()
   cy.wait('@getUserModels')
-  cy.contains('a','Liana') 
+
+/ cy.get('ul[class$="navbar-right"]')
+    .contains('Liana')
+    .parent()
+    .find('a[class="dropdown-toggle"]')
+    .click()
+ 
+  //busca todos os irmãos e busca o que tem img
+  cy.get('li[class^="dropdown"]')
+    .siblings()
+    .children('a')
+    .click()
+    cy.wait(1000)
+
+    //busca todos os irmãos e filtra
+    cy.get('li[class^="dropdown"]')
+    .siblings()
+    .children()
+    .filter('a')
+    .click()
+    cy.wait(1000)
+  //cy.contains('a','Liana') 
 })
